7decbf11d6e075e5bab9e9ac7b2e3c98e0ffc1f4
maiferna <maiderfernandez57@gmail.com>
2025-05-22 17:23:43 +0200

    ADD: async

---
+++ js/main.js
@@ -0,0 +1,78 @@
+/**
+ * Llamada a los elementos del DOM
+ */
+
+const infoUsuario = document.querySelector('#infoUsuario');
+const infoParrafo = document.querySelector('#infoParrafo');
+
+
+
+/**
+ * Evento
+ */
+
+document.addEventListener("click", (ev) => {
+    if (ev.target.matches('#btnDatos')) {
+        getApi();
+    }
+})
+
+
+/**
+ * Función que simula la api
+ * La promesa resuelta devuelve un objeto con los datos del usuario
+ * @returns Devuelve una promesa
+ */
+
+const api = () => {
+    let isOk = true;
+    const user = { name: 'Pepe', email: 'pepe@gmail.com' };
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (isOk) {
+                resolve(user);
+            } else {
+                reject('Error: No hay datos disponibles.')
+            }
+        }, 2000);
+    })
+    return (promise);
+}
+
+console.log(api());
+
+/**
+ * Función que resuelve la promesa
+ * try gestiona si todo va bien
+ * .catch() gestiona el error
+ */
+
+const getApi = async () => {
+    try {
+        const getApi = await api();
+        drawList(getApi);
+    } catch (error) {
+        infoParrafo.textContent = 'No hay datos disponibles.'
+    }
+}
+
+
+
+/**
+ * Función para pintar la lista
+ * @param {Object} user Objeto que devuelve el resolve
+ */
+
+const drawList = (user) => {
+    infoUsuario.innerHTML = '';
+    const listContainer = document.createElement("DIV");
+    const listBox = document.createElement("UL");
+    const listName = document.createElement("LI");
+    listName.textContent = `Nombre: ${user.name}`;
+    const listEmail = document.createElement("LI");
+    listEmail.textContent = `Email: ${user.email}`;
+
+    listBox.append(listName, listEmail);
+    listContainer.append(listBox);
+    infoUsuario.append(listContainer);
+}
